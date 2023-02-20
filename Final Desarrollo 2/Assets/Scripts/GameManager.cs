9b4240aa1dce34542098459b56ff7412d503a5e0
pichi02 <lucas.pich02@gmail.com>
2023-02-20 15:13:08 -0300

    Create GameManager script

---
+++ Final Desarrollo 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TankMovement tankMovement;
+    [SerializeField] private Countdown countdown;
+    [SerializeField] private ShootBehaviour shootBehaviour;
+    private void Start()
+    {
+        countdown.OnTimeFinish += tankMovement.DisableCanMove;
+        countdown.OnTimeFinish += shootBehaviour.DisableCanShoot;
+    }
+    private void OnDestroy()
+    {
+        countdown.OnTimeFinish += tankMovement.DisableCanMove;
+        countdown.OnTimeFinish += shootBehaviour.DisableCanShoot;
+    }
+}
